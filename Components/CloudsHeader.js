377cda601e52524f68b4d66bc1e9eac8f077c1e4
CandleLitScene <owen.hughes@unh.edu>
2025-03-08 12:43:11 -0500

    Misc Pages

---
+++ Components/CloudsHeader.js
@@ -0,0 +1,24 @@
+class Header extends HTMLElement {
+    constructor() {
+      super();
+    }
+
+  connectedCallback() {
+    this.innerHTML = `
+      <header>
+        <nav>
+            <ul>
+                <li><a class="link" href="/index.html">Homepage</a></li>
+                <li><a class="link" href="../Brideshead/brideshead.html">Brideshead Revisited</a></li>
+                <li><a class="link" href="Pentiment/Pentiment.html">Pentiment</a></li>
+                <li><a class="link" href="/My Work/MyWork.html">My Work</a></li>
+            </ul> 
+        </nav>
+        <h1>Candle-Lit Scene | Clouds</h1>
+        <p>This site is a continuous work in progress!</p>
+    </header>
+    `;
+  }
+}
+
+customElements.define('header-component', Header);
